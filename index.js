89b3efcc3e222be3dd4a9ae661271c8c8c26234f
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2020-02-07 11:34:24 -0500

    Add cap-support

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, 'Hello! I am BeaverBot and I am here to have fun and watch you guys progress in FRC!')
 })
 
-controller.hears(['\bhello\b', '\bheya\b', '\bhey\b', '\bhowdy\b', '\bgreetings\b', '\bhi\b'], ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
+controller.hears(['\bhello\b', '\bheya\b', '\bhey\b', '\bhowdy\b', '\bgreetings\b', '\bhi\b', '\bHello\b', '\bHeya\b', '\bHey\b', '\bHowdy\b', '\bGreetings\b', '\bHi\b'], ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
   await bot.reply(message, 'Hello!')
 })
 
